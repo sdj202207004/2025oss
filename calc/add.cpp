ffb088a71519891284fec3f6af994235828fb4f4
Keuin <rkdalstn3044@naver.com>
2025-06-19 11:25:03 +0900

    1. add.cpp , sub.cpp [ * ] 더하기 함수 및 빼기 함수 내용 작성

--- calc/add.cpp
+++ calc/add.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
 #include "add.h"
 
 int add(int x, int y)
 {
-	return 0;
+	printf("x[ %d ]와 y[ %d ] 값을 더합니다.\n");
+	return x + y;
 }
--- calc/sub.cpp
+++ calc/sub.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
 #include "sub.h"
 
 int sub(int x, int y)
 {
-	return 0;
+	printf("x[ %d ]에서 y[ %d ] 값을 뺍니다.\n");
+	return x - y;
 }
